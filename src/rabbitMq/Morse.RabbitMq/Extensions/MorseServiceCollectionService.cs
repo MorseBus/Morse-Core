59d02412f7c36d25721e5b89e0c2538856644c4b
Mohammad Shreem <m.shreem@t2.sa>
2020-10-29 14:30:32 +0300

    initialize test rabbit Mq publisher test

--- src/rabbitMq/Morse.RabbitMq/Extensions/MorseServiceCollectionService.cs
+++ src/rabbitMq/Morse.RabbitMq/Extensions/MorseServiceCollectionService.cs
@@ -1,24 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Morse.Abstractions.Services;
+using Morse.Core.Options;
 using Morse.Core.Services;
 
 namespace Morse.RabbitMq.Extensions
 {
     public static class MorseServiceCollectionService
     {
-        public static IServiceCollection AddMorsePublisher(this IServiceCollection services)
+        public static IServiceCollection AddMorseRabbitMqPublisher(this IServiceCollection services, IConfiguration configuration)
         {
+            return ConfigureRabbitMqServices(services, configuration);
+        }
+
+        public static IServiceCollection AddMorseRabbitMqConsumers(this IServiceCollection services, IConfiguration configuration)
+        {
+            return ConfigureRabbitMqServices(services, configuration);
+        }
+
+        private static IServiceCollection ConfigureRabbitMqServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MorseConnectionOptions>(options => configuration.GetSection(nameof(MorseConnectionOptions)).Bind(options));
             services.AddSingleton(typeof(IConnectionSelector), typeof(ConnectionSelector));
             services.AddSingleton(typeof(IQueueBuilder<>), typeof(QueueBuilder<>));
             services.AddSingleton(typeof(IMorsePublisher<>), typeof(RabbitMqPublisher<>));
-            return services;
-        }
-
-        public static IServiceCollection AddMorseConsumers(this IServiceCollection services)
-        {
-            services.AddSingleton(typeof(IConnectionSelector), typeof(ConnectionSelector));
-            services.AddSingleton(typeof(IQueueBuilder<>), typeof(QueueBuilder<>));
-            services.AddSingleton(typeof(IMorseConsumer<>), typeof(RabbitMqConsumer<>));
             return services;
         }
     }
---
+++ test/Morse.Tests/TestContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Morse.Tests
+{
+    public class TestContainer
+    {
+        protected TestContainer()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("testsettings.json")
+                .Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            ResolveServices();
+        }
+
+        protected IServiceProvider ServiceProvider { get; private set; }
+        protected IConfiguration Configuration { get; private set; }
+
+
+        protected virtual void ConfigureServices(IServiceCollection services) { }
+        protected virtual void ResolveServices() { }
+    }
+}
